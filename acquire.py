8839af213ce310941278e57078aaeeedeb217d00
Sam Callahan <callahan.l.sam@gmail.com>
2021-08-12 13:36:10 -0500

    May have acquired full site data. Running a full test now

--- acquire.py
+++ acquire.py
@@ -64,41 +64,104 @@ def archive_list(main_url):
 
     return archives
 
+def is_date(soup_element):
+    if soup_element.name == 'font' and soup_element.get('color') == '#660000' and soup_element.get('size') == '3':
+        return True
+    else:
+        return False
+
+def is_title(soup_element):
+    if soup_element.name == 'span' and soup_element.get('class') == ['textegrandfoncegras'] and '(' in soup_element.text:
+        return True
+    else:
+        return False
+
+def titles_and_dates(page_soup):
+    titles = []
+    date_soups = page_soup.find_all('font', color='#660000', size='3')
+
+    dates = [date_soup.text for date_soup in date_soups]
+
+    mess = page_soup.find_all(['font', 'span'])
+
+    cleaned = [i for i in mess if is_title(i) or is_date(i)]
+    for element in cleaned:
+        if element.name == 'font':
+            date = element.text
+        else:
+            titles.append({'title': element.text, 'date': date})
+    return titles
+
+def angus_list(page_soup):
+    angus_sections = page_soup.find_all('table', width='498')
+
+    angus = []
+
+    for section in angus_sections:
+        angus_soups = section.find_all('span', class_='textegrandfoncegras')
+        angus.extend([title.text for title in angus_soups if '(' in title.text])
+
+    return angus
+
+def author_by_title(title, angus_list):
+    if title in angus_list:
+        return 'Angus'
+    
+    else:
+        return 'Serge'
+    
+def ratings_list(page_soup):
+    rating_soups = page_soup.find_all('span', class_='textenormalgras')
+
+    ratings = [rating.text.strip() for rating in rating_soups if ' points.' in rating.text]
+    return ratings
+
+def contents_list(page_soup):
+    content_soups = page_soup.find_all('span', class_='TextenormalNEW')
+
+    contents = [content.text for content in content_soups if 'Colour' in content.text]
+
+    return contents
+
 def scrape_page(archive_url):
     session = get_session()
 
-    soup = BeautifulSoup(session.get(URL).content)
+    soup = BeautifulSoup(session.get(archive_url).content)
 
-    title_soups = soup.find_all('span', class_='textegrandfoncegras')
+    page = pd.DataFrame(titles_and_dates(soup))
 
-    titles = [title.text for title in title_soups if '(' in title.text]
-
-    angus_sections = soup.find_all('table', width='498')
-
-    angus_titles = []
-    
-    for section in angus_sections:
-        angus_soups = section.find_all('span', class_='textegrandfoncegras')
-        angus_titles.extend([title.text for title in angus_soups if '(' in title.text])
+    angus_titles = angus_list(soup)
 
     authors = []
 
-    for title in titles:
+    for title in page['title']:
         if title in angus_titles:
             authors.append('Angus')
         else:
             authors.append('Serge')
+    
+    page['author'] = page['title'].apply(lambda x: author_by_title(x, angus_titles))
 
-    rating_soups = soup.find_all('span', class_='textenormalgras')
+    page['rating'] = ratings_list(soup)
 
-    ratings = [rating.text.strip() for rating in rating_soups if ' points.' in rating.text]
+    page['content'] = contents_list(soup)
 
-    content_soups = soup.find_all('span', class_='TextenormalNEW')
+    feather.write_feather(df, f'data/{archive_url[25:-5]}.feather')
 
-    contents = [content.text for content in content_soups if 'Colour' in content.text]
+def all_pages(archives):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(scrape_page, archives)
 
-    date_soups = soup.find_all('font', color='#660000', size='3')
+def combine_feathers():
+    df = pd.DataFrame()
+    for feather in os.listdir('data'):
+        to_append = pd.read_feather(f'data/{feather}')
+        df = df.append(to_append, ignore_index=True)
+    return df
 
-    dates = [date_soup.text for date_soup in date_soups]
-
-    return 
+def whisky_df():
+    archives = archive_list(URL)
+    all_pages(archives)
+    whisky = combine_feathers()
+    whisky.to_csv('whiskyfun.csv')
+    return whisky
