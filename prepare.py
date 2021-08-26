dd0baa4710511c82b2778a1e63d167f152caf893
Sam Callahan <callahan.l.sam@gmail.com>
2021-08-26 14:32:33 -0500

    Added some planning for prep stage

---
+++ prepare.py
@@ -0,0 +1,13 @@
+'''
+plan:
+make columns:   distill year, bottle year, distillery, cask makeup, color, nose, palate, finish,
+                abv, rating, sgp, region
+maybe separate other types of liquors? rums, bourbons, yak, agave, etc
+maybe separate angus reviews?
+maybe regularize angus scale vs serge scale?
+standard NLP prep on each NLP column
+encode by distillery?
+encode region
+encode cask type?
+break up sgp into s, g, and p?
+'''
